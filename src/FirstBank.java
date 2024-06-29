98c21d52751e68a23d28615fdbdba8fd48908916
SatyajeetPPatil <149305491+SatyajeetPPatil@users.noreply.github.com>
2024-06-29 16:09:30 +0530

    Add files via upload

---
+++ src/FirstBank.java
@@ -0,0 +1,13 @@
+public class FirstBank {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		Account Satya = new Account("Satya", "S00002");
+		Satya.showMenu();
+		
+		
+	}
+
+}
